d2554890ef5c896211d5c45a611029caf8842969
Nick <nb00870@gmail.com>
2015-06-16 10:37:21 -0700

    Euphoria

---
+++ eos.mk
@@ -0,0 +1,47 @@
+# Boot animatio# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from f320 device
+$(call inherit-product, device/lge/f320/f320.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+
+PRODUCT_NAME := eos_f320
+PRODUCT_DEVICE := f320
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := LG-F320
+
+#PRODUCT_GMS_CLIENTID_BASE :=
+
+PRODUCT_BRAND := lge
+TARGET_VENDOR := lge
+TARGET_VENDOR_PRODUCT_NAME := f320
+TARGET_VENDOR_DEVICE_NAME := LG-F320
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=LG-F320 PRODUCT_NAME=f320
+
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+
+## Use the latest approved GMS identifiers unless running a signed build
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_skt_kr/g2:5.0.1/LRX21Y/f320S28d.1418740902:user/release-keys PRIVATE_BUILD_DESC="lge/g2_skt_kr/g2:5.0.1/LRX21Y/f320S28d.1418740902:user/release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
+
+
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_f320-userdebug
+add_lunch_combo eos_f320-userdebug
