5b4673cd9719a5519b664263b063f118d2791bd5
Nick <nb00870@gmail.com>
2015-06-16 11:29:30 -0700

    Switch target defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_f320_defconfig
+TARGET_KERNEL_CONFIG := f320_defconfig
 
 TARGET_OTA_ASSERT_DEVICE := f320,f320s,f320k,g2,galbi
 
